d5ad9ebc979ad89353aced07a468919f3ad00c86
zgrwo <zgrwo@163.com>
2026-06-25 02:29:36 +0800

    fix: RangeExportUdf/ArrayUdf补全using System.Runtime.InteropServices

--- src/DataToolkit/ArrayUdf.cs
+++ src/DataToolkit/ArrayUdf.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using ExcelDna.Integration;
 using ExcelVbaLibraries.Foundation;
 
--- src/DataToolkit/RangeExportUdf.cs
+++ src/DataToolkit/RangeExportUdf.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using System.Runtime.InteropServices;
 using ExcelDna.Integration;
 using ExcelVbaLibraries.Foundation;
 
